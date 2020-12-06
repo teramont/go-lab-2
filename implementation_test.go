d277a31a6c25e84311457e6a60c40c9141482e60
CheffChelos <baranikartur1008@gmail.com>
2020-12-06 10:58:00 +0200

    Add tests fot PostfixToPrefix and splitIntoTokens

--- implementation_test.go
+++ implementation_test.go
@@ -7,17 +7,47 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+func TestPostfixToPrefix(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"", ""},
+		{"18", "18"},
+		{"2 2 +", "+ 2 2"},
+		{"4 2 - 3 * var +", "+ * - 4 2 3 var"},
+		{"4 2-3*юнікод+", "+ * - 4 2 3 юнікод"},
+	}
+
+	for _, c := range cases {
+		res, err := PostfixToPrefix(c.in)
+		if assert.Nil(t, err) {
+			assert.Equal(t, c.want, res)
+		}
+	}
+
+	casesWrong := []string{
+		"+ 1",
+		"1 +",
+		"2 2 + 2 2 +",
+	}
+
+	for _, c := range casesWrong {
+		_, err := PostfixToPrefix(c)
+		if !assert.NotNil(t, err) {
+			t.Fatal("Not Nil")
+		}
 	}
 }
 
 func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+	res, _ := PostfixToPrefix("2 2 +")
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// + 2 2
+}
+
+func TestTokenizer(t *testing.T) {
+	res := splitIntoTokens("  123+456 /   978 ^variable-юнікод*  ")
+	assert.Equal(t, res, []string{"123", "+", "456", "/", "978", "^", "variable", "-", "юнікод", "*"})
 }
