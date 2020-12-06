6366afaecf247bcd4a3acb8190e11742f62baf95
teramont <rfibruk@gmail.com>
2020-12-06 10:17:27 +0200

    Add tokenizer

--- implementation.go
+++ implementation.go
@@ -1,8 +1,28 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
-// TODO: document this function.
+var r = regexp.MustCompile("\\+|\\*|\\/|\\-|\\^|[0-9]*")
+
+func splitIntoTokens(input string) []string {
+	r := strings.NewReplacer(
+		"+", " + ",
+		"-", " - ",
+		"/", " / ",
+		"*", " * ",
+		"^", " ^ ",
+	)
+
+	// Adding white characters to simplify token splitting
+	withWhitespaces := r.Replace(input)
+	tokens := strings.Fields(withWhitespaces)
+	return tokens
+}
+
 // PrefixToPostfix converts
 func PrefixToPostfix(input string) (string, error) {
 	return "TODO", fmt.Errorf("TODO")
